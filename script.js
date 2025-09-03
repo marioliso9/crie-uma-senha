646464cd8574f0cfc884392048210dfe7a7dcc63
marioliso9 <140639804+marioliso9@users.noreply.github.com>
2025-09-03 14:27:02 -0300

    000

--- script.js
+++ script.js
@@ -20,3 +20,38 @@ function aumentaTamanho (){
 numeroSenha.textContent = tamanhoSenha;
 geraSenha()
 }
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#%*?';
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+alfabeto = alfabeto + letrasMaiusculas
+    }
+     if (checkbox[1].checked){
+alfabeto = alfabeto + letrasMinusculas
+    }
+     if (checkbox[2].checked){
+alfabeto = alfabeto + numeros
+    }
+     if (checkbox[3].checked){
+alfabeto = alfabeto + simbolos
+    }
+    let senha = '';
+    for (let i = 0; 1 < tamanhoSenha; i++ ){
+    let numeroAleatorio = Math.random () * alfabeto.length;
+    numeroAleatorio = Math.floor (numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+}
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.LOG2E(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57){
+        
+    }
+}
