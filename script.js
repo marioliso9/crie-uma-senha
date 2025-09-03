158f0aea85fde31e11a5e44b85b7cce1f826dc88
marioliso9 <140639804+marioliso9@users.noreply.github.com>
2025-09-03 14:02:58 -0300

    000

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const botoes = document.querySelectorAll('parametro-senha-botao');
 const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho (){
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+numeroSenha.textContent = tamanhoSenha;
+geraSenha()
+}
+function aumentaTamanho (){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+numeroSenha.textContent = tamanhoSenha;
+geraSenha()
+}
